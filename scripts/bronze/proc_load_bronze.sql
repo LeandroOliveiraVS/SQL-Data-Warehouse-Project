7250ebfb3c2579ef0576d094529f52050d846226
Leandro <leandro.oliveiravs@gmail.com>
2026-02-17 10:34:09 -0300

    loading bronze data

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -4,3 +4,38 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE '/change/to_your/directory/sql-datawarehouse-project/datasets/source_crm/prd_info.csv'
+INTO TABLE bronze.crm_prd_info
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'change/to_your/directory/sql-datawarehouse-project/datasets/source_crm/sales_details.csv'
+INTO TABLE bronze.crm_sales_details
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'change/to_your/directory/sql-datawarehouse-project/datasets/source_erp/CUST_AZ12.csv'
+INTO TABLE bronze.erp_cust_az12
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'change/to_your/directory/sql-datawarehouse-project/datasets/source_erp/LOC_A101.csv'
+INTO TABLE bronze.erp_loc_a101
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'change/to_your/directory/sql-datawarehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+INTO TABLE bronze.erp_px_cat_g1v2
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
